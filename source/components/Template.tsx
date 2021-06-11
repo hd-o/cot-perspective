2d6cd6bd1f92df543ae0c661eaaea409bb7323ae
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2021-06-10 23:40:29 -0300

    Update Template.tsx

--- source/components/Template.tsx
+++ source/components/Template.tsx
@@ -49,13 +49,14 @@ export const Template = ({ tableData, dropDownsData }: TemplateProps): JSX.Eleme
     <html lang='en'>
       <head>
         <meta charSet='utf-8' />
-        <title>COT Perspective</title>
-        <meta name='description' content='A pleasant view on the COT reports' />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <title>COT Perspective • Delightful COT Reports</title>
+        <meta name='description' content='Delightful COT Reports' />
         <meta name='author' content='Hadrian de Oliveira' />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="COT Perspective" />
-        <meta property="og:title" content="COT Perspective" />
-        <meta property="og:description" content="A pleasant view on the COT reports" />
+        <meta property="og:title" content="COT Perspective • Delightful COT Reports" />
+        <meta property="og:description" content="Delightful COT Reports" />
         <meta property="og:image" content="https://cotperspective.com/preview.png" />
         <meta property="og:url" content="https://cotperspective.com" />
         <link rel='stylesheet' href='https://bootswatch.com/4/cosmo/bootstrap.min.css' />
