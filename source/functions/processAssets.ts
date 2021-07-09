64c9bfd4fe3d51b4b6546217ea88d7a132282c19
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2021-07-09 19:10:00 -0300

    Update processAssets.ts

--- source/functions/processAssets.ts
+++ source/functions/processAssets.ts
@@ -3,14 +3,11 @@ import fs from 'fs'
 import { buildPath } from '../data/buildPath'
 import { execSync } from 'child_process'
 
-const assetsPath = `${buildPath}/assets`
-
 export const processAssets = () => {
-  if (!fs.existsSync(assetsPath)) fs.mkdirSync(assetsPath)
   console.log('• Copying assets')
-  execSync(`cp -r ./source/assets ${assetsPath}`)
+  execSync(`cp -r ./source/assets ${buildPath}`)
   console.log('• Processing styles')
   const styles = '' + fs.readFileSync('./source/assets/styles.css')
   const minStyles = new CleanCSS().minify(styles).styles
-  fs.writeFileSync(`${assetsPath}/styles.css`, minStyles)
+  fs.writeFileSync(`${buildPath}/styles.css`, minStyles)
 }
