19f6d0693452d71d24393bd5a1a951300167a73e
Hadrian de Oliveira <hadrian.no.reply@outlook.com>
2021-07-09 18:58:27 -0300

    Update processAssets.ts
    Write styles to assets folder

--- source/functions/processAssets.ts
+++ source/functions/processAssets.ts
@@ -3,11 +3,14 @@ import fs from 'fs'
 import { buildPath } from '../data/buildPath'
 import { execSync } from 'child_process'
 
+const assetsPath = `${buildPath}/assets`
+
 export const processAssets = () => {
+  if (!fs.existsSync(assetsPath)) fs.mkdirSync(assetsPath)
+  console.log('• Copying assets')
+  execSync(`cp -r ./source/assets ${assetsPath}`)
   console.log('• Processing styles')
   const styles = '' + fs.readFileSync('./source/assets/styles.css')
   const minStyles = new CleanCSS().minify(styles).styles
-  fs.writeFileSync(`${buildPath}/styles.css`, minStyles)
-  console.log('• Copying assets')
-  execSync(`cp -r ./source/assets ${buildPath}/assets`, { stdio: 'inherit' })
+  fs.writeFileSync(`${assetsPath}/styles.css`, minStyles)
 }
